750de4d90ea7e70f0414db2aef83c4d0bea7b15d
YuliiaKirienko <varapapa@mail.ru>
2025-03-29 20:32:34 +0200

    test2

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,35 @@ public class Main {
         //1.
         printThreeWords();
         //2.
-        checkSumSign();
+        String result = checkSumSign();
+        System.out.println(result);
         //3.
-        printColor();
+        String result3 = printColor();
+        System.out.println(result3);
         //4.
-        compareNumbers();
+        String result4 = compareNumbers();
+        System.out.println(result4);
         //5.
-        SumAB();
+        int a = 4;
+        int b = 11;
+        boolean result5 = SumAB(a, b);
+        System.out.println(result5);
         //6.
-        NumberOne();
+        int number6 = -25;
+        NumberOne(number6);
         //7.
-        NumberTwo();
+        int number7 = 9;
+        boolean result7 = NumberTwo(number7);
+        System.out.println(result7);
         //8.
-        StringAndNumber();
+        String str = "Мама мыла пилораму";
+        int n = 3;
+        StringAndNumber(str, n);
         //9.
-        LeapYear();
+        int year = 1984;
+        boolean result9 = LeapYear(year);
+        System.out.println(result9);
+
 
         //10.
         int[] numbers = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
@@ -30,8 +44,7 @@ public class Main {
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == 0) {
                 numbers[i] = 1;
-            }
-            else {
+            } else {
                 numbers[i] = 0;
             }
 
@@ -45,7 +58,7 @@ public class Main {
         //11.
         int[] numbersnew = new int[100];
         for (int i = 0; i < 100; i++) {
-        numbersnew[i] = i + 1;
+            numbersnew[i] = i + 1;
         }
         System.out.println("Результат:");
         for (int i = 0; i < 100; i++) {
@@ -75,18 +88,17 @@ public class Main {
         System.out.println();
 
 
-
-       //13.
-        int n = 4;
+        //13.
+        int n13 = 4;
         int[][] cvadrat = new int[n][n];
         for (int i = 0; i < n; i++) {
-        cvadrat[i][i] = 1;
+            cvadrat[i][i] = 1;
         }
         for (int row = 0; row < n; row++) {
-        for (int col = 0; col < n; col++) {
-        System.out.print(cvadrat[row][col] + " ");
+            for (int col = 0; col < n; col++) {
+                System.out.print(cvadrat[row][col] + " ");
             }
-        System.out.println();
+            System.out.println();
         }
 
         //14.
@@ -97,7 +109,7 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
-        System.out.println();
+            System.out.println();
     }
 
 
@@ -110,95 +122,76 @@ public class Main {
     }
 
     //2.1.
-    public static void checkSumSign() {
+    public static String checkSumSign() {
         int a = 105;
         int b = 54;
         int sum = a + b;
         if (sum >= 0) {
-            System.out.println("Сумма положительная");
-        } else  {
-            System.out.println("Сумма отрицательная");
+            return "Сумма положительная";
+        } else {
+            return "Сумма отрицательная";
         }
     }
 
     //3.1.
-    public static void printColor() {
+    public static String printColor() {
         int value = 122;
         if (value <= 0) {
-            System.out.println("Красный");
-        } else if (value > 0 && value <= 100) {
-            System.out.println("Желтый");
-        } else if (value > 100) {
-            System.out.println("Зеленый");
+            return "Красный";
+        } else if (value <= 100) {
+            return "Желтый";
+        } else {
+            return "Зеленый";
         }
     }
+
     //4.1
-    public static void compareNumbers() {
+    public static String compareNumbers() {
         int a = 19;
         int b = 8;
         if (a >= b) {
-            System.out.println("а >= b");
+            return "а >= b";
         } else {
-            System.out.println("а < b");
+            return "а < b";
         }
     }
 
     //5.1.
-    public static boolean SumAB() {
-        int a = 4;
-        int b = 11;
-        int sum = a + b;
-        if (sum >= 10 && sum <= 20) {
-            System.out.println("true");
-            return true;
-        } else {
-            System.out.println("false");
-            return false;
-        }
+    public static boolean SumAB(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum >= 10 && sum <= 20;
     }
+
     //6.1.
-    public static void NumberOne() {
-        int number = - 25;
+    public static void NumberOne(int number) {
         if (number >= 0) {
             System.out.println("Положительное");
         } else {
             System.out.println("Отрицательное");
         }
     }
-//7.1.
-    public static boolean NumberTwo() {
-        int number = 16;
-        if (number < 0) {
-            System.out.println("true");
-            return true;
-        } else {
-            System.out.println("false");
-            return false;
-        }
+
+    //7.1.
+    public static boolean NumberTwo(int number7) {
+        return number7 < 0;
     }
 
     //8.1
-    public static void StringAndNumber() {
-        String str = "Мама мыла пилораму ";
-        int n = 3;
+    public static void StringAndNumber(String str, int n) {
         for (int i = 0; i < n; i++) {
             System.out.println(str);
         }
     }
     //9.1
-    public static boolean LeapYear() {
-        int year = 1984;
+    public static boolean LeapYear(int year) {
         if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                return year % 400 == 0;
-            }
-            System.out.println (year + "- високосный год");
-            return true;
+        if (year % 100 == 0) {
+        return year % 400 == 0;
         }
-        System.out.println (year + "- НЕ високосный год");
-            return false;
-    }
-
+         return true;
+         }
+         return false;
+        }
     //10.1 в 10.
     //11.1 в 11.
     //12.1 в 12.
