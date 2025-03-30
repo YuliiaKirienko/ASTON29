a6098cef199bf9b83bdc44d993617c9276d3d20a
YuliiaKirienko <varapapa@mail.ru>
2025-03-30 12:37:01 +0200

    Lesson_65

---
+++ src/Product.java
@@ -0,0 +1,29 @@
+// задача 1
+import java.time.LocalDate;
+
+public class Product {
+    private String name;
+    private LocalDate productionDate;
+    private String manufacturer;
+    private String countryOfOrigin;
+    private double price;
+    private boolean bookingStatus;
+
+    public Product(String name, LocalDate productionDate, String manufacturer, String countryOfOrigin, double price, boolean bookingStatus) {
+        this.name = name;
+        this.productionDate = productionDate;
+        this.manufacturer = manufacturer;
+        this.countryOfOrigin = countryOfOrigin;
+        this.price = price;
+        this.bookingStatus = bookingStatus;
+    }
+
+    public void printProductInfo() {
+        System.out.println("Название: " + this.name);
+        System.out.println("Дата производства: " + this.productionDate);
+        System.out.println("Производитель: " + this.manufacturer);
+        System.out.println("Страна происхождения: " + this.countryOfOrigin);
+        System.out.println("Цена: " + this.price);
+        System.out.println("Состояние бронирования покупателем: " + (this.bookingStatus ? "Забронирован" : "Доступен"));
+    }
+}
