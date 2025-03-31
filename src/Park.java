9d4c257d6f391f713c52f65d981268119a877089
YuliiaKirienko <varapapa@mail.ru>
2025-03-31 16:20:17 +0200

    new

---
+++ src/Park.java
@@ -0,0 +1,32 @@
+public class Park {
+    private String name;
+
+    public Park(String name) {
+        this.name = name;
+    }
+
+    public class Attraction {
+        private final String name;
+        private final String workingHours;
+        private final double price;
+
+        public Attraction(String name, String workingHours, double price) {
+            this.name = name;
+            this.workingHours = workingHours;
+            this.price = price;
+        }
+
+        public void printAttractionInfo() {
+            System.out.println("Информация об аттракционе: ");
+            System.out.println("Название: " + this.name);
+            System.out.println("Часы работы: " + this.workingHours);
+            System.out.println("Стоимость: " + this.price);
+        }
+
+
+
+
+    }
+
+
+}
