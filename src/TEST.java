8b3ef4d275fc2fdffd2f37e6f73485c724c7d7c8
YuliiaKirienko <varapapa@mail.ru>
2025-04-11 13:20:43 +0200

    09.04

--- src/TEST.java
+++ src/TEST.java
@@ -1,2 +1,15 @@
 public class TEST {
+    private static int animalCount = 0;
+    protected String name;
+    protected int maxRunDistance;
+    protected int maxSwimDistance;
+    protected boolean canSwim;
+
+    public Animal(String name, int maxRunDistance, int maxSwimDistance, boolean canSwim) {
+        this.name = name;
+        this.maxRunDistance = maxRunDistance;
+        this.maxSwimDistance = maxSwimDistance;
+        this.canSwim = canSwim;
+        animalCount++;
+    }
 }
