ebba65d9486c71383552a156cab4a00d755f8fd9
YuliiaKirienko <varapapa@mail.ru>
2025-04-17 22:37:32 +0300

    Delete src/MyArrayDataException.java

--- src/MyArrayDataException.java
+++
@@ -1,5 +0,0 @@
-public class MyArrayDataException extends Exception {
-    public MyArrayDataException(int row, int col) {
-        super(String.format("Невозможно преобразовать данные в ячейке [%d][%d]", row, col));
-    }
-}
